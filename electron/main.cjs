dfe56a34142256fd964e98db4c24d40faeb65fed
Renso <rensovargas@gmail.com>
2026-06-26 00:10:15 +0100

    fix: use COEP credentialless to allow model-viewer iframe in Electron

---
+++ electron/main.cjs
@@ -0,0 +1,40 @@
+const { app, BrowserWindow, session } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
+    callback({
+      responseHeaders: {
+        ...details.responseHeaders,
+        'Cross-Origin-Opener-Policy': ['same-origin'],
+        'Cross-Origin-Embedder-Policy': ['credentialless'],
+      },
+    });
+  });
+
+  const win = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.cjs'),
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  if (app.isPackaged || process.env.NODE_ENV === 'production') {
+    win.loadFile(path.join(__dirname, '../dist/index.html'));
+  } else {
+    win.loadURL('http://localhost:4000');
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
